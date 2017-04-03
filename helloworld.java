d9077cdad2f29331dcb3c85481e6f70f639490c4
vamshi493 <vamshimysigari@gmail.com>
2017-04-03 22:22:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-  public class helloworld {
-    public static void main(string[] args) {
-      system.out.println("bolo helloworld Munchkins-GITHUB");
+  public class HelloWorld {
+        public static void main(string[] args) {
+      system.out.println("bolo Hello World Munchkins-GITHUB");
   }
   }
